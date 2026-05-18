af2ab22fdaeb2e26283f3ff077a5b26e8ddb9a44
Anacleto <jpanacleto5@gmail.com>
2026-05-18 19:20:47 -0300

    bug fix kwargs

---
+++ tests/unit_tests/test_generic_static_abc_contract.py
@@ -0,0 +1,17 @@
+import inspect
+from genericparser.plugins.domain.generic_class import GenericStaticABC
+
+
+def test_extract_abstract_method_uses_var_keyword_args():
+    """
+    Garante que o ABC declara a variael kwargs do extract VAR_KEYWORD.
+    """
+    sig = inspect.signature(GenericStaticABC.extract)
+    params = list(sig.parameters.values())
+
+    # Remove 'self'
+    non_self_params = [p for p in params if p.name != "self"]
+
+    assert len(non_self_params) == 1
+    assert non_self_params[0].kind == inspect.Parameter.VAR_KEYWORD
+    
